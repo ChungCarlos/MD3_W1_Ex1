c47b7ce5b6b31876d5d31fe7b09353893b8dff88
Chung <Joschungcarlos95@gmail.com>
2023-05-08 13:37:32 +0700

    EX truy van CSDL

--- QuanLySinhVien.sql
+++ QuanLySinhVien.sql
@@ -77,3 +77,26 @@ WHERE C.ClassName = 'A1';
 SELECT S.StudentId, S.StudentName, Sub.SubName, M.Mark
 FROM Student S join Mark M on S.StudentId = M.StudentId join Subject Sub on M.SubId = Sub.SubId
 WHERE Sub.SubName = 'CF';
+
+-- Hiển thị tất cả các sinh viên có tên bắt đầu bảng ký tự ‘h’--
+select * from Student where StudentName like 'h%';
+
+-- Hiển thị các thông tin lớp học có thời gian bắt đầu vào tháng 12 --
+select class.ClassName, class.StartDate
+from class
+where month(class.StartDate)  >= 12;
+
+-- Hiển thị tất cả các thông tin môn học có credit trong khoảng từ 3-5 --
+select * from subject
+where Credit >=3 and Credit <= 5;
+
+-- Thay đổi mã lớp(ClassID) của sinh viên có tên ‘Hung’ là 2 --
+update student set ClassID = 2
+where StudentName like '%Hung';
+
+-- Hiển thị các thông tin: StudentName, SubName, Mark. Dữ liệu sắp xếp theo điểm thi (mark) giảm dần. nếu trùng sắp theo tên tăng dần.
+select student.StudentName, subject.SubName, mark.Mark
+from mark
+join student on student.StudentID = mark.StudentID
+join subject on subject.SubID = mark.SubID
+order by mark.Mark desc, student.StudentName asc;
