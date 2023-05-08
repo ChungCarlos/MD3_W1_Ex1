f68ec4ce95c8d9d6557cd1ce155d4c93dcb27842
Chung <Joschungcarlos95@gmail.com>
2023-05-08 10:47:18 +0700

    Truy Van CSDL

--- QuanLySinhVien.sql
+++ QuanLySinhVien.sql
@@ -35,22 +35,45 @@ create table Subject(
  modify StartDate date;
  
  
- insert into Class (ClassName,StartDate,Status) values ('A1','2008-12-20',1);
- insert into Class (ClassName,StartDate,Status) values ('A2','2008-12-22',1);
- insert into Class (ClassName,StartDate,Status) values ('B3',current_date,0);
+ insert into Class (ClassName,StartDate,Status) 
+ values ('A1','2008-12-20',1),
+		('A2','2008-12-22',1),
+		('B3',current_date,0);
  
- insert into Student (StudentID,StudentName,Address,Phone,Status,ClassID) values (1,'Hung','Ha noi','0912113113',1,1);
- insert into Student (StudentID,StudentName,Address,Phone,Status,ClassID) values (2,'Hoa','Hai Phong','',1,1);
- insert into Student (StudentID,StudentName,Address,Phone,Status,ClassID) values (3,'Manh','HCM','0123123123',0,2);
+ insert into Student (StudentID,StudentName,Address,Phone,Status,ClassID)
+ values (1,'Hung','Ha noi','0912113113',1,1),
+		(2,'Hoa','Hai Phong','',1,1),
+		(3,'Manh','HCM','0123123123',0,2);
  
- insert into subject (SubName,Credit,Status) values ('CF',5,1);
- insert into subject (SubName,Credit,Status) values ('C',6,1);
- insert into subject (SubName,Credit,Status) values ('HDJ',5,1);
- insert into subject (SubName,Credit,Status) values ('RDBMS',10,1);
+ insert into subject (SubName,Credit,Status) 
+ values ('CF',5,1),
+		('C',6,1),
+		('HDJ',5,1),
+		('RDBMS',10,1);
  
- insert into Mark (SubID,StudentID,Mark,ExamTimes) values (1,1,8,1);
- insert into Mark (SubID,StudentID,Mark,ExamTimes) values (1,2,10,2);
- insert into Mark (SubID,StudentID,Mark,ExamTimes) values (2,1,12,1);
+ insert into Mark (SubID,StudentID,Mark,ExamTimes)
+ values (1,1,8,1),
+		(1,2,10,2),
+		(2,1,12,1);
  
+ SELECT *
+FROM Student;
+
+SELECT *
+FROM Student
+WHERE Status = true;
+
+SELECT *
+FROM Subject
+WHERE Credit < 10;
  
- 
+ SELECT S.StudentId, S.StudentName, C.ClassName
+FROM Student S join Class C on S.ClassId = C.ClassID;
+
+SELECT S.StudentId, S.StudentName, C.ClassName
+FROM Student S join Class C on S.ClassId = C.ClassID
+WHERE C.ClassName = 'A1';
+
+SELECT S.StudentId, S.StudentName, Sub.SubName, M.Mark
+FROM Student S join Mark M on S.StudentId = M.StudentId join Subject Sub on M.SubId = Sub.SubId
+WHERE Sub.SubName = 'CF';
