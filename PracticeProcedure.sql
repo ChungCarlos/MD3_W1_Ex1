4111b5c1e5ff00c1f64dd1c13833b15bff967999
Chung <Joschungcarlos95@gmail.com>
2023-05-10 14:42:27 +0700

    TH procedure

---
+++ PracticeProcedure.sql
@@ -0,0 +1,9 @@
+delimiter //
+drop procedure if exists findAllCustomer //
+create procedure  findAllCustomer()
+begin
+    select  * from  customers where customerNumber = 175;
+end //
+delimiter ;
+
+call findAllCustomer();
